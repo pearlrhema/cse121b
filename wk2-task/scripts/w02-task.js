9b823ba99c92145f9d3d8798c576a0c68aad92b0
pearlrhema <pearlrhema8@gmail.com>
2023-09-24 20:15:45 +0100

    week-2 programming task

---
+++ wk2-task/scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Godwin Akpan Essien';
+let currentYear = '2023';
+let profilePicutre = 'C:\Users\DELL\Documents\Godwin_BYUI\byui\cse121b\cse121b-new\me.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicutre);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+//imageElement.querySelector('img').innerHTML = profilePicutre;
+
+/* Step 5 - Array */
+let favoriteFoods = ['Afang Soup', 'Ekpang', 'rice']
+let favFood = ['atoo, ikpong'];
+favoriteFoods.push(favFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.splice(0, 1);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
