3a88eb0af9c694a6899193e157966773aa0cd50c
pearlrhema <pearlrhema8@gmail.com>
2023-10-03 22:00:58 +0100

    week 4 assignment

--- weekly task/scripts/w04-task.js
+++ weekly task/scripts/w04-task.js
@@ -1,27 +1,49 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-
-
-
+let myProfile = {
+    name: 'Godwin Akpan Essien',
+    photo: 'images/me.jpg',
+    favoriteFoods: ['Afang Soup', 'Ekpang', 'rice', 'Banana Cream Pie'],
+    hobbies: ['Scripture Reading', 'Playing keyboard', 'Singing'],
+    placesLived: [''] 
+};
 
 /* Populate Profile Object with placesLive objects */
-
+let myPlace = {
+    place: 'Ikot Ekpene, Akwa Ibom',
+    length: '20 years',
+    place: 'Portharcourt',
+    length: '1 month',
+    place: 'Cross River',
+    length: '2 weeks'
+}
+myProfile.placesLived.push(myPlace);
 
 
 
 /* DOM Manipulation - Output */
 
 /* Name */
-
+document.querySelector('#name').textContent = myProfile.name;
 /* Photo with attributes */
-
+const imageElement = document.querySelector('#photo');
+imageElement.src = myProfile.photo;
+imageElement.setAttribute('alt', `Profile image of ${myProfile.name}`);
 
 /* Favorite Foods List*/
-
+myProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+});
 
 /* Hobbies List */
-
+myProfile.hobbies.forEach(hobbie => {
+    let li = document.createElement('ul');
+    li.textContent = hobbie;
+    document.querySelector('#hobbies').appendChild(li);
+});
 
 /* Places Lived DataList */
 
